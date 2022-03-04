c28d9cdff394fe43ea75e56f7101f58789cd3c0e
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2022-03-04 20:44:48 -0300

    Deploy

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify'
 Vue.use(IconsPlugin)
 
